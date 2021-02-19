3453cb76e843a7a356f5e82a650b11aef8117682
terahje <terahjequinn@gmail.com>
2021-02-19 15:06:59 -0700

    updated landing page

--- client/src/components/landingpage.js
+++ client/src/components/landingpage.js
@@ -1,10 +1,38 @@
 import React, { Component } from 'react';
+import { Grid, Cell } from 'react-mdl';
 
 class Landing extends Component {
     render() {
         return(
-            <div>
-                <h1>Landing Page</h1>
+            <div style={{width: '100%', margin: 'auto'}}>
+                <Grid className="landing-grid">
+                    <Cell col={12}>
+                    <img
+                    src={require("../assets/images/profile-pic.jpg").default}
+					alt="Terahje Gratkowski"
+                    className= "avatar-img"
+                    />
+                    <div className="banner-text">
+                        <h1>Full-Stack Web Developer</h1>
+
+                        <hr/>
+
+                        <p>HTML/CSS | Bootstrap | JavaScript | React | Pyton | AWS | NodeJS | Express | MongoDB</p>
+
+                    <div className="social-links">
+                        {/* LinkedIn */}
+                        <a href="https://www.linkedin.com/in/terahje-gratkowski/" rel="noopener noreferrer" target="_blank">
+                            <i className="fa fa-linkedin-square" aria-hidden="true" />
+                        </a>
+
+                        {/* Github */}
+                        <a href="https://github.com/terahje" rel="noopener noreferrer" target="_blank">
+                            <i className="fa fa-github-square" aria-hidden="true" />
+                        </a>
+                    </div>
+                    </div>
+                    </Cell>
+                </Grid>
             </div>
         )
     }
