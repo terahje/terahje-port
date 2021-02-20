bbc49ffb518d0603c94ec4727b007ff0d70ef432
terahje <terahjequinn@gmail.com>
2021-02-19 22:20:22 -0700

    added cards for personal projects, will need to come back and fill out projects

--- client/src/components/projects.js
+++ client/src/components/projects.js
@@ -1,10 +1,96 @@
 import React, { Component } from 'react';
+import { Tabs, Tab, Grid, Cell, Card, CardTitle, CardText, CardActions, Button, CardMenu, IconButton } from 'react-mdl';
 
 class Projects extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { activeTab: 0 };
+    }
+
+    toggleCategories() {
+
+        if(this.state.activeTab === 0){
+          return(
+            <div className="projects-grid">
+              {/* Project 1 */}
+              <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+                <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://images.unsplash.com/photo-1581276879432-15e50529f34b?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2700&q=80) center / cover'}} >React Project #1</CardTitle>
+                <CardText>
+                  Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
+                </CardText>
+                <CardActions border>
+                  <Button colored>GitHub</Button>
+                  
+                  <Button colored>Live Demo</Button>
+                </CardActions>
+                <CardMenu style={{color: '#fff'}}>
+                  <IconButton name="share" />
+                </CardMenu>
+              </Card>
+    
+              {/* Project 2 */}
+              <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+                <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://images.unsplash.com/photo-1585011650347-c59dbef5a823?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2700&q=80) center / cover'}} >React Project #2</CardTitle>
+                <CardText>
+                  Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
+                </CardText>
+                <CardActions border>
+                  <Button colored>GitHub</Button>
+                  
+                  <Button colored>Live Demo</Button>
+                </CardActions>
+                <CardMenu style={{color: '#fff'}}>
+                  <IconButton name="share" />
+                </CardMenu>
+              </Card>
+    
+              {/* Project 3 */}
+              <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+                <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://images.unsplash.com/photo-1607853827120-6847830b38b0?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=881&q=80) center / cover'}} >React Project #3</CardTitle>
+                <CardText>
+                  Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
+                </CardText>
+                <CardActions border>
+                  <Button colored>GitHub</Button>
+                  
+                  <Button colored>Live Demo</Button>
+                </CardActions>
+                <CardMenu style={{color: '#fff'}}>
+                  <IconButton name="share" />
+                </CardMenu>
+              </Card>
+            </div>
+    
+          )} else if(this.state.activeTab === 1) {
+            return (
+              <div><h1>This is React</h1></div>
+            )
+          } else if(this.state.activeTab === 2) {
+            return (
+              <div><h1>This is JS</h1></div>
+            )
+          } else if(this.state.activeTab === 3) {
+            return (
+              <div><h1>This is Python</h1></div>
+            )
+          }
+    }
+
     render() {
         return(
-            <div>
-                <h1>Projects Page</h1>
+            <div className="category-tabs">
+                <Tabs activeTab={this.state.activeTab} onChange={(tabId) => this.setState({ activeTab: tabId })} ripple>
+                    <Tab>All</Tab>
+                    <Tab>React</Tab>
+                    <Tab>JavaScript</Tab>
+                    <Tab>Python</Tab>
+                </Tabs>
+
+                <Grid>
+                    <Cell col={12}>
+                    <div className="content">{this.toggleCategories()}</div>
+                    </Cell>
+                </Grid>
             </div>
         )
     }
