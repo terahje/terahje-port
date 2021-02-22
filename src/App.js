a98186121307d166ff54d25907ca8e38b1757be0
terahje <terahjequinn@gmail.com>
2021-02-21 23:48:40 -0700

    made some updates to app.js

--- src/App.js
+++ src/App.js
@@ -1,37 +1,39 @@
+import React, { Component } from 'react';
 import './App.css';
-import { Layout, Header, Navigation, Drawer,Content } from 'react-mdl';
+import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
 import Main from './components/main';
 import { Link } from 'react-router-dom';
 
-function App() {
+class App extends Component {
+    render() {
   return (
     <div className="demo-big-content">
-    <Layout>
-        <Header className="header-color" title="Terahje's Portfolio" scroll>
-            <Navigation> 
-                <Link to="/home" className="nav-color">Home</Link>
-                <Link to="/resume" className="nav-color">Resume</Link>
-                <Link to="/aboutme" className="nav-color">About Me</Link>
-                <Link to="/projects" className="nav-color">Projects</Link>
-                <Link to="/contact" className="nav-color">Contact </Link>
-            </Navigation>
-        </Header>
-        <Drawer title="Terahje's Portfolio">
-            <Navigation className="nav-color">
-                <Link to="/home">Home</Link>
-                <Link to="/resume">Resume</Link>
-                <Link to="/aboutme">About Me</Link>
-                <Link to="/projects">Projects</Link>
-                <Link to="/contact">Contact </Link>
-            </Navigation>
-        </Drawer>
-        <Content>
-            <div className="page-content" />
-            <Main></Main>
-        </Content>
-    </Layout>
-</div>
-  );
+        <Layout>
+            <Header className="header-color" title="Terahje's Portfolio" scroll>
+                <Navigation> 
+                    <Link to="/home" className="nav-color">Home</Link>
+                    <Link to="/resume" className="nav-color">Resume</Link>
+                    <Link to="/aboutme" className="nav-color">About Me</Link>
+                    <Link to="/projects" className="nav-color">Projects</Link>
+                    <Link to="/contact" className="nav-color">Contact </Link>
+                </Navigation>
+            </Header>
+            <Drawer title="Terahje's Portfolio">
+                <Navigation className="nav-color">
+                    <Link to="/home">Home</Link>
+                    <Link to="/resume">Resume</Link>
+                    <Link to="/aboutme">About Me</Link>
+                    <Link to="/projects">Projects</Link>
+                    <Link to="/contact">Contact </Link>
+                </Navigation>
+            </Drawer>
+            <Content>
+                <div className="page-content" />
+                <Main></Main>
+            </Content>
+        </Layout>
+    </div>
+    );
+  }
 }
-
 export default App;
